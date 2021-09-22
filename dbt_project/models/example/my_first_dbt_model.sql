26c2dbe4bcb5a57df658e132a70bcbce9a91cf31
smegnshd <smegnshdem@gmail.com>
2021-09-22 19:11:24 +0300

    add table

--- dbt_project/models/example/my_first_dbt_model.sql
+++ dbt_project/models/example/my_first_dbt_model.sql
@@ -9,19 +9,38 @@
 
 {{ config(materialized='table') }}
 
-with source_data as (
+-- with source_data as (
 
-    select 1 as id
-    union all
-    select null as id
+    -- select 1 as id
+    --union all
+    --select null as id
 
+--)
+
+--select *
+--from source_data
+--*/
+with source as (
+  select
+   * 
+ from {{ ref('station_summary') }}
+),
+
+stage_station as (
+  select
+    ID,
+    flow_99,
+    flow_max,
+    flow_median,
+    flow_total,
+    n_obs,
+  from source
 )
-
-select *
-from source_data
-
-/*
+select 
+  *
+from stage_station
+--/*
     Uncomment the line below to remove records with null `id` values
-*/
+-- */
 
 -- where id is not null
